17273a543ddbfa01c06ba30407d5899036131597
Ming Wang <wangmingjob@gmail.com>
2014-06-30 15:36:20 +0800

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
       if @user.save
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
-          render 'new'
+        render 'new'
       end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only:[:new, :create, :destroy]
 
   # get 'static_pages/home'
   # 
@@ -14,4 +15,7 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+  match '/signin',  to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
